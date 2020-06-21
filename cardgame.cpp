2482c1ba0bf723797343bee73a1d3424f90175f2
Rishabh Dutt <rishabh.dutt@citrix.com>
2020-06-21 12:41:31 +0530

    Codeforces Problem

---
+++ cardgame.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+
+/*sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);*/
+
+template < class c > struct rge { c b, e; };
+template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+	~debug() { cerr << endl; }
+
+	//eni(!=) cerr << boolalpha << i; ris; }        Part 1
+
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+		cerr << boolalpha << i;
+		return * this;
+	}
+
+	//eni(==) ris << range(begin(i), end(i)); }     Part 2
+
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+		return * this << range(begin(i), end(i));
+	}
+
+	/*sim, class b dor(pair < b, c > d) {           Part 3
+		ris << "(" << d.first << ", " << d.second << ")";
+	}*/
+
+	template < class c, class b > debug & operator << (pair < b, c > d) {
+		return * this << "(" << d.first << ", " << d.second << ")";
+	}
+
+	/*sim dor(rge<c> d) {                           Part 4
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		ris << "]";
+	}*/
+
+	template < class c > debug & operator <<(rge<c> d) {
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		return * this << "]";
+	}
+
+};
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define ll long long int
+#define pb push_back
+#define loop(i,n) for(i=0;i<n;i++)
+#define loopk(i, n, k) for(i=k;i<n;i++)
+
+template<typename... T>
+void read(T&... args) {
+	((cin >> args), ...);
+}
+
+template<typename... T>
+void write(T... args) {
+	((cout << args << " "), ...);
+}
+
+void testcase() {
+    int n;
+    read(n);
+    string s;
+    read(s);
+    int czero = 0, cone=0;
+    for(auto it=s.cbegin();it!=s.cend();it++) {
+        if(*it == 'z') czero++;
+        if(*it == 'n') cone++;
+    }
+    int i;
+    loop(i, cone) write(1);
+    loop(i, czero) write(0);
+    cout << endl;
+}
+
+
+int main() {
+	testcase();
+	return 0;
+}
---
+++ dieroll.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+
+/*sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);*/
+
+template < class c > struct rge { c b, e; };
+template < class c > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+template < class c > auto dud(c* x) -> decltype(cerr << *x, 0);
+template < class c > char dud(...);
+
+struct debug {
+	~debug() { cerr << endl; }
+
+	//eni(!=) cerr << boolalpha << i; ris; }        Part 1
+
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) != 1, debug&>::type operator<<(c i) {
+		cerr << boolalpha << i;
+		return * this;
+	}
+
+	//eni(==) ris << range(begin(i), end(i)); }     Part 2
+
+	template < class c > typename \
+	enable_if<sizeof dud<c>(0) == 1, debug&>::type operator<<(c i) {
+		return * this << range(begin(i), end(i));
+	}
+
+	/*sim, class b dor(pair < b, c > d) {           Part 3
+		ris << "(" << d.first << ", " << d.second << ")";
+	}*/
+
+	template < class c, class b > debug & operator << (pair < b, c > d) {
+		return * this << "(" << d.first << ", " << d.second << ")";
+	}
+
+	/*sim dor(rge<c> d) {                           Part 4
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		ris << "]";
+	}*/
+
+	template < class c > debug & operator <<(rge<c> d) {
+		*this << "[";
+		for (auto it = d.b; it != d.e; ++it)
+			*this << ", " + 2 * (it == d.b) << *it;
+		return * this << "]";
+	}
+
+};
+
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define ll long long int
+#define pb push_back
+#define loop(i,n) for(i=0;i<n;i++)
+#define loopk(i, n, k) for(i=k;i<n;i++)
+
+template<typename... T>
+void read(T&... args) {
+	((cin >> args), ...);
+}
+
+template<typename... T>
+void write(T... args) {
+	((cout << args << " "), ...);
+}
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
+void testcase() {
+    int a,b;
+    read(a, b);
+    int max_elem = max(a,b);
+    int num=0;
+    for(int i=max_elem;i<=6;i++) num++;
+    int denom=6;
+    int hcf = gcd(denom, num);
+    denom = denom/hcf;
+    num = num/hcf;
+    printf("%d/%d\n", num, denom);
+}
+
+
+int main() {
+	testcase();
+	return 0;
+}
